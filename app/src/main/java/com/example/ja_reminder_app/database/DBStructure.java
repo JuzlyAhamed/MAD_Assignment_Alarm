99fedb7e5ed425e48e1af89c9df551f55fe0d982
Juzly Ahamed <juzlyahamed8@gmail.com>
2021-10-13 19:14:19 +0530

    database configuration

---
+++ app/src/main/java/com/example/ja_reminder_app/database/DBStructure.java
@@ -0,0 +1,10 @@
+package com.example.ja_reminder_app.database;
+
+public class DBStructure {
+
+    //Constants for db name and version
+    public static final String DATABASE_NAME = "EVENTS_DB";
+    public static final int DATABASE_VERSION = 4;
+
+}
+
